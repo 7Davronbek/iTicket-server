3db41ada03281cad822e05b55964708f99a1af60
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-09-13 00:30:16 +0300

    iTicket created

---
+++ src/main/java/com/example/iticket/ITicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.iticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iticket/ITicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
