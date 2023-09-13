b17d1e96837c8a991fff841386fe468d045e831a
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-09-13 22:59:54 +0300

    iTicket app

--- src/main/java/com/example/iticket/user/service/UserService.java
+++ src/main/java/com/example/iticket/user/service/UserService.java
@@ -44,3 +44,88 @@ public class UserService implements CommonService<UUID, UserResponseDto, UserUpd
         return null;
     }
 }
+
+
+/**
+ * package com.example.demo.user;
+ *
+ * import com.example.demo.user.dto.UserCreateDTO;
+ * import com.example.demo.user.dto.UserResponseDTO;
+ * import com.example.demo.user.dto.UserUpdateDTO;
+ * import com.example.demo.user.entity.User;
+ * import org.springframework.stereotype.Service;
+ *
+ * import java.util.LinkedHashMap;
+ * import java.util.List;
+ * import java.util.UUID;
+ *
+ * @Service
+ * public class UserService {
+ *     private final LinkedHashMap<UUID, User> users = new LinkedHashMap<>();
+ *
+ *     public void create(UserCreateDTO userCreateDTO) {
+ *
+ *         User user = new User();
+ *
+ *         user.setId(UUID.randomUUID());
+ *         user.setName(userCreateDTO.getName());
+ *         user.setUsername(userCreateDTO.getUsername());
+ *         user.setSurname(userCreateDTO.getSurname());
+ *         user.setEmail(userCreateDTO.getEmail());
+ *         user.setPassword(userCreateDTO.getPassword());
+ *
+ *         users.put(user.getId(), user);
+ *     }
+ *
+ *     public List<UserResponseDTO> getUsers() {
+ *         return users.values()
+ *                 .stream()
+ *                 .map(user -> {
+ *                     UserResponseDTO userResponseDTO = new UserResponseDTO();
+ *
+ *                     userResponseDTO.setId(user.getId());
+ *                     userResponseDTO.setName(user.getName());
+ *                     userResponseDTO.setSurname(user.getSurname());
+ *                     userResponseDTO.setUsername(user.getUsername());
+ *                     userResponseDTO.setEmail(user.getEmail());
+ * //                    new ResponseEntity<>("User updated successfully", HttpStatus.CREATED);
+ *                     return userResponseDTO;
+ *                 }).toList();
+ *     }
+ *
+ *     public UserResponseDTO getUser(UUID id) {
+ *         User user = users.get(id);
+ *         if (user == null) return null;
+ *
+ *         UserResponseDTO userResponseDTO = new UserResponseDTO();
+ *
+ *         userResponseDTO.setEmail(user.getEmail());
+ *         userResponseDTO.setSurname(user.getSurname());
+ *         userResponseDTO.setUsername(user.getUsername());
+ *         userResponseDTO.setName(user.getName());
+ *         userResponseDTO.setId(user.getId());
+ *
+ *         return userResponseDTO;
+ *     }
+ *
+ *     public void update(UUID id, UserUpdateDTO userUpdateDTO) {
+ *         User user = users.get(id);
+ *         if (user == null) {
+ *             return;
+ *         }
+ *
+ *         user.setEmail(userUpdateDTO.getEmail());
+ *         user.setSurname(userUpdateDTO.getSurname());
+ *         user.setUsername(userUpdateDTO.getUsername());
+ *         user.setName(userUpdateDTO.getName());
+ *         user.setPassword(userUpdateDTO.getPassword());
+ *
+ *         users.put(id, user);
+ * //        new ResponseEntity<>("User updated successfully", HttpStatus.CREATED);
+ *     }
+ *
+ *     public void delete(UUID id) {
+ *         users.remove(id);
+ *     }
+ * }
+ * */
